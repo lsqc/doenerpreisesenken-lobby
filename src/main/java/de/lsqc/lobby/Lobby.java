c59f0aa3ef177ae73d29a32619523f4cc4a7c5b5
lsqc <lsqc@nya.vodka>
2025-09-13 16:18:18 +0200

    update

--- src/main/java/de/lsqc/lobby/Lobby.java
+++ src/main/java/de/lsqc/lobby/Lobby.java
@@ -1,21 +1,35 @@
 package de.lsqc.lobby;
 
+import java.io.File;
+
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import de.lsqc.lobby.listeners.PlayerJoinQuitListener;
+import lombok.Getter;
 
 public final class Lobby extends JavaPlugin
 {
 
+    @Getter
+    private static Lobby instance;
+
+    @Getter
+    private YamlConfiguration yamlConfig;
+
     @Override
     public void onEnable()
     {
+        instance = this;
 
         this.getServer().getMessenger().registerOutgoingPluginChannel(this, "velocity:player");
-
         this.registerListeners();
+
+        this.getDataFolder().mkdirs();
+
+        this.yamlConfig = YamlConfiguration.loadConfiguration(new File(getDataFolder(), "config.yml"));
     }
 
     @Override
